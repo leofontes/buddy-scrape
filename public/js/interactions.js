be937e62e5805d370c16af1339106415e3c807d0
Leonardo Fontes <leofontes@gmail.com>
2017-08-24 15:52:38 -0300

    version 1.0

--- public/js/interactions.js
+++ public/js/interactions.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
 
   $('#scrapeButton').click(function() {
-    var html = $('#inputTextarea').val();
-    scrape(html);
-  });
+    var html = $('#inputTextarea').val()
+    var scrapedData = scrape(html)
 
-});
+    $('#outputClassNames').append(stringifyArray(scrapedData.classNames))
+    $('#outputPercentages').append(stringifyArray(scrapedData.percents))
+  })
+
+})
+
+function stringifyArray(arr) {
+  var string = ""
+
+  for(var i = 0; i < arr.length; i++) {
+    string += arr[i] + "<br>"
+  }
+
+  return string
+}
--- public/js/scraper.js
+++ public/js/scraper.js
@@ -1,3 +1,30 @@
 function scrape(html) {
-  
+  var words = html.split("\n")
+  var filtered = words.filter(filterBusiness)
+  filtered.sort()
+
+  return getPercentArray(filtered)
+}
+
+function filterBusiness(value) {
+  return value.indexOf("Business") != -1
+}
+
+function getPercentArray(arr) {
+  var data = {
+    classNames : [],
+    percents: []
+  }
+
+  for(var i = 0; i < arr.length; i++) {
+    var indexOfSpace = arr[i].indexOf(" ")
+    data.classNames.push(arr[i].substring(0, indexOfSpace))
+    data.percents.push(replaceDots(arr[i].substring(indexOfSpace + 2)))
+  }
+
+  return data
+}
+
+function replaceDots(percentage) {
+  return percentage.replace(".", ",")
 }
